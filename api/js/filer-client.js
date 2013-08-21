f8d8cd3d669fa5b09e4c85199e1aa5ae09482c93
Alan Kligman <ack@mozilla.com>
2013-08-21 12:09:12 -0400

    wip

---
+++ api/js/filer-client.js
@@ -0,0 +1,67 @@
+(function() {
+
+  var origin = window.location.origin.toString();
+  var targetOrigin = "http://filer.io";
+
+  var iframe = document.createElement("IFRAME");
+  iframe.setAttribute("src", targetOrigin + "/iframe/?origin=" + origin);
+  iframe.style.display = "none";
+  document.body.appendChild(iframe);
+
+  var queue = {};
+
+  function guid() {
+    return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+      var r = Math.random()*16|0, v = c == 'x' ? r : (r&0x3|0x8);
+      return v.toString(16);
+    }).toUpperCase();
+  };
+
+  function Request(method, args) {
+    this.id = guid();
+    this.transfer = [];
+
+    args.forEach(function(arg) {
+      if(arg instanceof ArrayBuffer) {
+        this.transfer.push(arg);
+      }
+    });
+
+    this.message = {
+      method: method,
+      args: args
+    };
+  };
+
+  function sendMessage(rpc, callback) {
+    iframe.postMessage(rpc.message, targetOrigin, rpc.transfer);
+    queue[rpc.id] = callback;
+  };
+
+  function receiveMessage(event) {
+    if(targetOrigin !== event.origin ||
+       iframe !== event.source) {
+      return;
+    }
+
+    var data = event.data;
+    if(!queue.hasOwnProperty(data.id)) {
+      return;
+    }
+
+    queue[data.id].apply(undefined, data.args);
+    delete queue[data.id];
+  };
+
+  function FileSystemProxy() {
+  };
+  FileSystemProxy.prototype.stat = function stat(path, callback) {
+    sendMessage(new Request("stat", [path]));
+  };
+
+  iframe.addEventListener('load', function() {
+    window.filer = new FileSystemProxy();
+  });
+
+  window.addEventListener('message', receiveMessage, false);
+})();
--- iframe/js/filer-iframe.js
+++ iframe/js/filer-iframe.js
@@ -1,10 +1,44 @@
-window.addEventListener('load', function() {
+(function() {
+  window.addEventListener('load', function() {
 
-	var fs = new IDBFS.FileSystem('filer');
+    var origin = "http://filer.io";
+    var targetOrigin = window.location.toString().split('?')[1];
+  	var fs = new IDBFS.FileSystem('filer');
 
-  function handleMessage(event) {
+    function Response(args) {
+      this.id = guid();
+      this.transfer = [];
 
-  };
+      args.forEach(function(arg) {
+        if(arg instanceof ArrayBuffer) {
+          this.transfer.push(arg);
+        }
+      });
 
-	window.addEventListener('message', handleMessage, false);
-});
+      this.message = {
+        method: method,
+        args: args
+      };
+    };
+
+    function sendMessage(target, rpc, callback) {
+      target.postMessage(rpc.message, targetOrigin, rpc.transfer);
+    };
+
+    function receiveMessage(event) {
+      if(targetOrigin !== event.origin) {
+        return;
+      }
+
+      var source = event.source;
+      var data = event.data;
+      fs[data.message.method].apply(undefined, data.message.args,
+        function() {
+          sendMessage(source, new Response())
+        }
+      );
+    };
+
+  	window.addEventListener('message', receiveMessage, false);
+  });
+})();
